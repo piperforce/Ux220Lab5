e85180634ddf865724d7d5c2a4a63e10f04dcbe5
piperforce <piperforce@gmail.com>
2023-11-08 21:06:24 -0500

    new custom element

--- customelement.js
+++ customelement.js
@@ -5,3 +5,29 @@ this.innerHTML = "Piper was here";
 }
 
 customElements.define("x-piper", PiperElement);
+
+class CenterElement extends HTMLElement{
+connectedCallback(){
+this.innerHTML = `<div style="text-align:center">${this.innerHTML}</div>
+`;
+}
+}
+
+customElements.define("x-center", CenterElement);
+
+class YearElement extends HTMLElement{
+connectedCallback(){
+this.innerHTML = new Date().getFullYear();
+}
+}
+
+customElements.define("x-fullyear", YearElement);
+
+class ColorElement extends HTMLElement{
+connectedCallback(){
+this.innerHTML = `<div style="color:red">${this.innerHTML}</div>
+`;
+}
+}
+
+customElements.define("x-red", ColorElement);
